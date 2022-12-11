e43d82a4f8395b84380393d82d95279cdc3f9ea7
hmutugi <neckbone010@gmail.com>
2022-12-11 03:19:06 -0800

    c program that prints various types of computer sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+include < stdio.h >
+/**
+ * main -entry
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("size of char: %i byte(s)\n", size of(char));
+	printf("size of int: %i byte(s)\n", size of(int));
+	printf("size of long long int: %i byte(s)\n", size of(long long int));
+	printf("size of long int: %i byte(s)\n", size of(long int));
+	printf("size of float int: %i byte(s)\n", size of(float int));
+	return (0);
+}
+
